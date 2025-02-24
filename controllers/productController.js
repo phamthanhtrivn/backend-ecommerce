85456ed6ca3b99fc61c2f89d7e07b504fb0bd73b
Tri Pham <phamthanhtri0712@gmail.com>
2025-02-24 10:03:41 +0700

    first commit

---
+++ controllers/productController.js
@@ -0,0 +1,22 @@
+
+// function for add product
+const addProduct = async (req, res) => {
+
+}
+
+// function for list product
+const listProduct = async (req, res) => {
+
+}
+
+// function for reomving product
+const removeProduct = async (req, res) => {
+
+}
+
+// function for single product info
+const singleProduct = async (req, res) => {
+
+}
+
+export { listProduct, addProduct, removeProduct, singleProduct }
